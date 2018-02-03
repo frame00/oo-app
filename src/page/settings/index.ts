1c7b262e72c965d025ec6b150933114e46806557
aggre <hiroyuki.aggre@gmail.com>
2018-02-03 17:05:32 +0900

    Fix style

--- src/page/settings/index.ts
+++ src/page/settings/index.ts
@@ -33,20 +33,32 @@ export default (paths: Array<string>): CallbackOptions => {
 	})}
 	<main>
 		<oo-profile-editor></oo-profile-editor>
-		<article>
-			<h2>Medium</h2>
-			<oo-button data-iam=@IAM@></oo-button>
-			<div class=code>
-				<span>${escape('<script async src="//elements.ooapp.co/stable/oo-button.js"></script>')}</span>
-				<span data-inject-iam>${escape('<oo-button data-iam="@IAM@"></oo-button>')}</span>
-			</div>
+		<article class=info>
+			<section>
+				<h2>Add Double O Button to your site.</h2>
+				<p>Medium</p>
+				<oo-button data-iam=@IAM@></oo-button>
+				<div class=code>
+					<span>${escape('<script async src="//elements.ooapp.co/stable/oo-button.js"></script>')}</span>
+					<span data-inject-iam>${escape('<oo-button data-iam="@IAM@"></oo-button>')}</span>
+				</div>
 
-			<h2>Small</h2>
-			<oo-button data-size=small data-iam=@IAM@></oo-button>
-			<div class=code>
-				<span>${escape('<script async src="//elements.ooapp.co/stable/oo-button.js"></script>')}</span>
-				<span data-inject-iam>${escape('<oo-button data-size="small" data-iam="@IAM@"></oo-button>')}</span>
-			</div>
+				<p>Small</p>
+				<oo-button data-size=small data-iam=@IAM@></oo-button>
+				<div class=code>
+					<span>${escape('<script async src="//elements.ooapp.co/stable/oo-button.js"></script>')}</span>
+					<span data-inject-iam>${escape('<oo-button data-size="small" data-iam="@IAM@"></oo-button>')}</span>
+				</div>
+			</section>
+		</article>
+		<article class=danger>
+			<section>
+				<h2>Delete account</h2>
+				<p>Once you delete an account, you can not use the same account again.</p>
+				<p>Please check if there are any ongoing projects.</p>
+				<p>Would you like to delete your account?</p>
+				<oo-delete-account></oo-delete-account>
+			</section>
 		</article>
 		${_footer()}
 	</main>
