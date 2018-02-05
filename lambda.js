592b1570a2d792d970a8c347874a7c0e017ed3fb
aggre <hiroyuki.aggre@gmail.com>
2018-02-05 11:09:18 +0900

    Change to Lambda event model

--- lambda.js
+++ lambda.js
@@ -2,7 +2,7 @@ const lambda = require('./dist/lambda')
 
 const [, , url] = process.argv
 const event = {
-	path: url
+path: url.replace(/\?.*/, '')
 }
 
 lambda.handler(event, null, (err, result) => {
