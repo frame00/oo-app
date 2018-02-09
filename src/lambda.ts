04e8dce8c6e60f474706a1372396e34af7469056
aggre <hiroyuki.aggre@gmail.com>
2018-02-09 19:22:29 +0900

    Fixed problem when tail was /

--- src/lambda.ts
+++ src/lambda.ts
@@ -2,7 +2,7 @@ import app from './index'
 import {APIGatewayEvent, ProxyCallback} from 'aws-lambda'
 
 export const handler = async (event: APIGatewayEvent, _, callback: ProxyCallback): Promise<void> => {
-	const paths = event.path.replace(/^\//, '').split('/')
+	const paths = event.path.replace(/^\//, '').replace(/\/$/, '').split('/')
 	const result = await app(paths)
 	const {err = null, status: statusCode, body} = result
 	callback(err, {
