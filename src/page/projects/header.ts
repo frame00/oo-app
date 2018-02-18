888b09c08f1a01293716566f4df54ccfd64873a9
aggre <hiroyuki.aggre@gmail.com>
2018-02-18 17:29:12 +0900

    Change style of projects page

--- src/page/projects/header.ts
+++ src/page/projects/header.ts
@@ -3,8 +3,18 @@ import onProjectCreated from '../../lib/on-project-created'
 export default () => {
 	return `
 	<style>
-		oo-ask-with-sign-in {
-			max-width: 500px;
+		.wrap {
+			width: 100%;
+			padding: 3rem;
+			background: #FFEB3B;
+			margin-bottom: 3rem;
+			h1 {
+
+			}
+		}
+		.form {
+			margin: auto;
+			max-width: 700px;
 		}
 		.message {
 			margin-bottom: 1rem;
@@ -19,11 +29,14 @@ export default () => {
 			}
 		}
 	</style>
-	<div style="margin-top: 3rem">
-		<div class=message>
-			<input disabled placeholder="Tags (, separated) 👷soon..."></input>
+	<div class=wrap>
+		<div class=form>
+			<h1>Any questions?</h1>
+			<div class=message>
+				<input disabled placeholder="Tags (, separated) 👷soon..."></input>
+			</div>
+			<oo-ask-with-sign-in data-sign-in-flow=redirect on-projectcreated></oo-ask-with-sign-in>
 		</div>
-		<oo-ask-with-sign-in data-sign-in-flow=redirect on-projectcreated></oo-ask-with-sign-in>
 	</div>
 	${onProjectCreated}
 	`
