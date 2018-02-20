70f15e735e0afa3befdbae6fffbfed81cd234ad5
aggre <hiroyuki.aggre@gmail.com>
2018-02-20 13:49:06 +0900

    Fixed a bug that failed sign in

--- src/lib/exp-iam.ts
+++ src/lib/exp-iam.ts
@@ -2,7 +2,7 @@ export default `
 <script>
 	(() => {
 		const uid = window.localStorage.getItem('oo:uid')
-		if(!uid) return window.location.href = '/sign'
+		if(!uid || uid === 'undefined') return window.location.href = '/sign'
 		window.document.querySelectorAll('[data-iam="@IAM@"]').forEach(el => {
 			el.setAttribute('data-iam', uid)
 		})
--- src/page/sign/script.ts
+++ src/page/sign/script.ts
@@ -1,7 +1,8 @@
 export default `
 <script>
 	function signedIn() {
-		window.location.href = '/dashboard'
+		const uid = window.localStorage.getItem('oo:uid')
+		if(uid && uid !== 'undefined') window.location.href = '/dashboard'
 	}
 	window.document.querySelectorAll('oo-sign-in-with-redirect').forEach(el => {
 		el.addEventListener('signedin', signedIn)
