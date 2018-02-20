d4e47a9ef285f6146b054fcc0735b8816c655d6a
aggre <hiroyuki.aggre@gmail.com>
2018-02-20 10:45:41 +0900

    Delete dummy parameters

--- src/page/project/on-message-sent.ts
+++ src/page/project/on-message-sent.ts
@@ -4,12 +4,12 @@ import og from '../../lib/og-image'
 const dummyImage = 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=='
 
 export default (paths: Array<string>, uid: string, count: number) => {
-	if (count < 1) {
+	if (count < 100) {
 		return `
 		<script>
 			(() => {
 				window.document.querySelector('[on-messagesent]').addEventListener('messagesent', e => {
-					const og = '${og('project', uid, ~~(Math.random() * 10000))}'
+					const og = '${og('project', uid, ~~(count + 1))}'
 					const modal = document.getElementById('answerShare')
 					const img = modal.querySelector('img')
 					img.src = og
