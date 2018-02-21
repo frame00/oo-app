ede7dbd050eb188e73da14c6f6a17d0bed1d38d9
aggre <hiroyuki.aggre@gmail.com>
2018-02-21 14:37:38 +0900

    Changed variable name

--- src/lib/exp-iam.ts
+++ src/lib/exp-iam.ts
@@ -1,11 +1,12 @@
-export default (removeWhenSignedout: boolean = false) => {
+export default (remove: boolean = false) => {
 	return `
 	<script>
-		((removeWhenSignedout) => {
+		((r) => {
 			const uid = window.localStorage.getItem('oo:uid')
 			const removeEl = el => el.parentNode.removeChild(el)
-			const remove = removeWhenSignedout && !uid
-			if(!remove) if(!uid || uid === 'undefined') return window.location.href = '/sign'
+			const signedIn = uid && uid !== 'undefined'
+			const remove = r && !signedIn
+			if(!remove && !signedIn) return window.location.href = '/sign'
 			window.document.querySelectorAll('[data-iam="@IAM@"]').forEach(el => {
 				if(remove) removeEl(el)
 				else el.setAttribute('data-iam', uid)
@@ -18,7 +19,7 @@ export default (removeWhenSignedout: boolean = false) => {
 				if(remove) removeEl(el)
 				else el.textContent = el.textContent.replace('@IAM@', uid)
 			})
-		})(${removeWhenSignedout})
+		})(${remove})
 	</script>
 	`
 }
