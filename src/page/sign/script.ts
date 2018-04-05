fe1b59fc09a38c1681c78717143861b908722e0a
aggre <hiroyuki.aggre@gmail.com>
2018-04-05 13:27:40 +0900

    Change redirect URL after signed-in to user page

--- src/page/sign/script.ts
+++ src/page/sign/script.ts
@@ -2,7 +2,7 @@ export default `
 <script>
 	function signedIn() {
 		const uid = window.localStorage.getItem('oo:uid')
-		if(uid && uid !== 'undefined') window.location.href = '/dashboard'
+		if(uid && uid !== 'undefined') window.location.href = '/' + uid
 	}
 	window.document.querySelectorAll('oo-sign-in-with-redirect').forEach(el => {
 		el.addEventListener('signedin', signedIn)
