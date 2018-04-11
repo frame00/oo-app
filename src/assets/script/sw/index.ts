7f28fd020f38226b22f3aff3a7f79660a78977fa
aggre <hiroyuki.aggre@gmail.com>
2018-04-11 14:31:50 +0900

    No cache Service Worker in browser

--- src/assets/script/sw/index.ts
+++ src/assets/script/sw/index.ts
@@ -1,9 +1,17 @@
 import {readFileSync} from 'fs'
 import {resolve, join} from 'path'
+import {CallbackOptions} from '../../../type/callback'
 
-export default () => {
+export default (): CallbackOptions => {
 	const root = resolve(__dirname)
 	const sw = join(root, './sw.js')
 	const body = readFileSync(sw, 'utf-8')
-	return body
+	return {
+		headers: {
+			'content-type': 'application/javascript; charset=UTF-8',
+			'cache-control': 'max-age=0,no-cache,no-store,must-revalidate'
+		},
+		body,
+		status: 200
+	}
 }
