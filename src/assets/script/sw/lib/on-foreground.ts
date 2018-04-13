42eeb9c6313b21ff244c55f37328ee5af1cd933c
aggre <hiroyuki.aggre@gmail.com>
2018-04-13 18:22:26 +0900

    Parallelizing the processing that can be parallel processing

--- src/assets/script/sw/lib/on-foreground.ts
+++ src/assets/script/sw/lib/on-foreground.ts
@@ -5,16 +5,19 @@ import sendToClient from './send-to-client'
 export default (e: ClientToSWMessageEvent, cacheName: string) => {
 	const handler = async () => {
 		const requests = urls.map(url => new Request(url))
-		const results = await Promise.all(
-			requests.map(request => fetch(request))
-		)
-		const cachedResults = await Promise.all<Response>(requests.map(request => {
-			return caches.match(request)
-		}))
+		const resolvedItems = await Promise.all([
+			Promise.all(
+				requests.map(request => fetch(request))
+			),
+			Promise.all<Response>(
+				requests.map(request => caches.match(request))
+			),
+			caches.open(cacheName)
+		])
+		const [results, cachedResults, cache] = resolvedItems
 		const texts = await Promise.all(cachedResults.map((cachedResult, i) => {
 			return Promise.all([cachedResult.clone().text(), results[i].clone().text()])
 		}))
-		const cache = await caches.open(cacheName)
 		const operation = await Promise.all(texts.map(async (text, i) => {
 			const [prev, next] = text
 			if (prev.length === next.length) {
